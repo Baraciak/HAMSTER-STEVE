6908376fb77d7350bb755ed9c877d35082c018e5
Baraciak <baraciak@gmail.com>
2019-04-25 12:55:39 +0200

    added dragNdrop on gameboard, side bars for pipes, footer

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,8 @@
+function main(){
+    dragula([document.getElementById('pipes-container-left'),
+            document.getElementById('board'),
+            document.getElementById('pipes-container-right')]);
+}
 
+
+main();
